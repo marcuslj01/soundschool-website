82fb326b4405c67a3ac3f96b695c3efc1a0bc97a
marcuslj <marcuslj@stud.ntnu.no>
2025-04-04 12:57:46 +0200

    feat: confetti fun

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
+import Image from "next/image";
+import confetti from "canvas-confetti";
+
 export default function Home() {
+  const handleClick = () => {
+    confetti({
+      particleCount: 150,
+      spread: 90,
+      origin: { y: 0.6 },
+    });
+  };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Halla Philip</h1>
+      <h1 className="text-4xl font-bold">Halla Philip</h1>
+      <Image
+        src="/Philip.png"
+        alt="Philip"
+        className="rounded-full"
+        width={500}
+        height={500}
+      />
+      <button
+        onClick={handleClick}
+        className="px-6 py-3 bg-pink-600 text-white font-semibold rounded-xl hover:bg-pink-700 transition"
+      >
+        Trykk her
+      </button>
       <p>Nå är det dags att skriva om min portfolio.</p>
     </div>
   );
